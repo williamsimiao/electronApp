9c0a1f2345d2967b63513a90ed04d10ad0385d23
William Simiao <williamsimiao@gmail.com>
2020-01-20 18:35:02 -0300

    initial commit

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, ipcMain, dialog, Menu} = require('electron')
 const path = require('path')
+
+// process.env.NODE_ENV = 'development'
+process.env.NODE_ENV = 'production'
+
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -27,7 +31,74 @@ function createWindow () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    mainWindow = null
+    // mainWindow = null
+    app.quit()
+  })
+  // Build menu from template
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate)
+  // Insert menu
+  Menu.setApplicationMenu(mainMenu)
+}
+
+const mainMenuTemplate =  [
+  // Each object is a dropdown
+  {
+    label: 'File',
+    submenu:[
+      {
+        label:'Set Env var',
+        click(){
+          console.log(process.env.HOME)
+          process.env.MY_ENV_VAR = 'teste'
+          console.log(process.env.MY_ENV_VAR)
+        }
+      },
+      {
+        label:'Open file dialog',
+        click(){
+          dialog.showOpenDialog({
+            properties: ['openFile', 'openDirectory']
+          }, (files) => {
+            console.log('HI')
+            if (files) {
+              console.log(files)
+              document.getElementById('selected-dir').innerHTML = `You selected: ${files}`
+              // event.sender.send('selected-directory', files)
+            }
+          })
+        }
+      },
+      {
+        label: 'Quit',
+        accelerator:process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+        click(){
+          app.quit();
+        }
+      }
+    ]
+  }
+]
+// If OSX, add empty object to menu
+// if(process.platform === 'darwin'){
+//   mainMenuTemplate.unshift({});
+// }
+
+// Add developer tools option if in dev
+if(process.env.NODE_ENV !== 'production'){
+  mainMenuTemplate.push({
+    label: 'Developer Tools',
+    submenu:[
+      {
+        role: 'reload'
+      },
+      {
+        label: 'Toggle DevTools',
+        accelerator:process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
+        click(item, focusedWindow){
+          focusedWindow.toggleDevTools();
+        }
+      }
+    ]
   })
 }
 
--- renderer.js
+++ renderer.js
@@ -4,3 +4,15 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+const {ipcRenderer} = require('electron')
+
+const selectDirBtn = document.getElementById('selectfile')
+
+selectDirBtn.addEventListener('click', (event) => {
+  console.log('click recebido')
+  ipcRenderer.send('open-file-dialog')
+})
+
+ipcRenderer.on('selected-directory', (event, path) => {
+  document.getElementById('selected-dir').innerHTML = `You selected: ${path}`
+})
